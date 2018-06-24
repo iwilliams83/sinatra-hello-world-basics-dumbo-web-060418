8bde2cb5d7613f606bbb27964a003e219e34c41e
iwilliams83 <indirajamurath@gmail.com>
2018-06-24 17:11:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
     puts "Hello, World!"
-  end 
+  end
 
 end
